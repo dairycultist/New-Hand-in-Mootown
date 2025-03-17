6ce30e491d29b29fafb0dd1c1cf45cff0cc85570
BlueBunnex <75032462+BlueBunnex@users.noreply.github.com>
2025-03-17 02:24:47 -0400

    Add picking up stuff

---
+++ chip_spawner.gd
@@ -0,0 +1,13 @@
+extends Node3D
+
+const CHIP = preload("res://chip.tscn")
+
+func _ready() -> void:
+	
+	for x in 4:
+		for y in 4:
+			for z in 4:
+				var chip := CHIP.instantiate()
+				add_child(chip)
+				
+				chip.global_position = global_position + Vector3(x, y, z)
--- player_controller.gd
+++ player_controller.gd
@@ -1,7 +1,6 @@
 extends CharacterBody3D
 
 @export var hold_anchor : Node3D
-@export var held : RigidBody3D
 @export var camera : Camera3D
 @export var mouse_sensitivity := 0.3
 @export var walk_speed := 5
@@ -9,12 +8,10 @@ extends CharacterBody3D
 @export var max_fall_speed := 32
 
 var camera_pitch := 0.0
+var held : RigidBody3D
 
 func _ready() -> void:
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-	
-	held.linear_damp = 10
-	held.gravity_scale = 0
 
 func _process(delta: float) -> void:
 	
@@ -44,10 +41,21 @@ func _process(delta: float) -> void:
 
 func _input(event):
 	
-	if (event.is_action_released("interact") && held):
-		held.linear_damp = 0
-		held.gravity_scale = 1
-		held = null
+	if (event.is_action_released("interact")):
+		
+		if held:
+			held.linear_damp = 0
+			held.gravity_scale = 1
+			held = null
+		else:
+			var ray_result = get_world_3d().direct_space_state.intersect_ray(
+				PhysicsRayQueryParameters3D.create(camera.global_position, hold_anchor.global_position)
+			)
+			
+			if ray_result.has("collider") and ray_result.collider is RigidBody3D:
+				held = ray_result.collider
+				held.linear_damp = 10
+				held.gravity_scale = 0
 	
 	if event.is_action_pressed("pause"):
 		
