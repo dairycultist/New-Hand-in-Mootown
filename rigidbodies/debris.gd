4b6956d761293ca229e924d7748f28bd2145f378
BlueBunnex <75032462+BlueBunnex@users.noreply.github.com>
2025-03-17 05:17:37 -0400

    Debris now fully randomly initializes

--- rigidbodies/debris.gd
+++ rigidbodies/debris.gd
@@ -1,16 +1,26 @@
 extends RigidBody3D
 
-@export var mesh : MeshInstance3D
-@export var collider : CollisionShape3D
-
 const CUBE_MESH = preload("res://rigidbodies/cube.obj")
 const DODEC_MESH = preload("res://rigidbodies/dodecahedron.obj")
 
+const MATERIAL = preload("res://rigidbodies/debris.tres")
+
 func _ready() -> void:
+	
+	var rand = RandomNumberGenerator.new()
+	
+	var mesh := MeshInstance3D.new()
+	var collider := CollisionShape3D.new()
+	add_child(mesh)
+	add_child(collider)
+	
+	var unique_material = MATERIAL.duplicate()
+	unique_material.albedo_color = Color.from_ok_hsl(rand.randf(), 0.7, 0.7, 1.0)
+	mesh.material_override = unique_material
 	
 	mass = 10
 	
-	match RandomNumberGenerator.new().randi_range(0, 3):
+	match rand.randi_range(0, 3):
 		0:
 			mesh.mesh = CapsuleMesh.new()
 			collider.shape = CapsuleShape3D.new()
