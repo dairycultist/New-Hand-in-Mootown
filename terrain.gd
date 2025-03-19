a5c9a4744d20e57d9ab018c3458c8d18d02f750b
BlueBunnex <75032462+BlueBunnex@users.noreply.github.com>
2025-03-19 17:29:51 -0400

    Noisy Terrain

--- terrain.gd
+++ terrain.gd
@@ -1,6 +1,8 @@
-extends MeshInstance3D
+extends StaticBody3D
 
 @export var SIZE := 128
+@export var TEX : NoiseTexture2D
+@export var MAT : Material
 
 # https://docs.godotengine.org/en/stable/tutorials/3d/procedural_geometry/arraymesh.html#doc-arraymesh
 # look into heightmapshape for collision
@@ -16,9 +18,12 @@ func _ready():
 	var indices = PackedInt32Array()
 	
 	# generate mesh
+	await TEX.changed
+	var image := TEX.get_image()
+	
 	for x in SIZE:
 		for z in SIZE:
-			verts.append(Vector3(x, 0, z))
+			verts.append(Vector3(x, image.get_pixel(x, z).r * 4, z))
 			normals.append(Vector3(0, 1, 0))
 			uvs.append(Vector2(x, z))
 	
@@ -41,5 +46,8 @@ func _ready():
 	surface_array[Mesh.ARRAY_NORMAL] = normals
 	surface_array[Mesh.ARRAY_INDEX] = indices
 
-	mesh = ArrayMesh.new()
-	mesh.add_surface_from_arrays(Mesh.PRIMITIVE_TRIANGLES, surface_array)
+	var mesh_instance := MeshInstance3D.new()
+	mesh_instance.mesh = ArrayMesh.new()
+	mesh_instance.mesh.add_surface_from_arrays(Mesh.PRIMITIVE_TRIANGLES, surface_array)
+	mesh_instance.set_surface_override_material(0, MAT)
+	add_child(mesh_instance)
