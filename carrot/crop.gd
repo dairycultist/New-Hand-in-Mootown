1d63a81fdeb2590d49d27ca57cf5d5e63d3ebe7f
BlueBunnex <75032462+BlueBunnex@users.noreply.github.com>
2025-06-24 01:47:18 -0400

    Working on crops

---
+++ carrot/crop.gd
@@ -0,0 +1,8 @@
+extends Node
+
+# wait until the object has a really high upward force on it
+# before unfreezing
+
+func _ready() -> void:
+	# move down into ground, then do a random slight rotation
+	pass
