099c280d1c296993f93f2818dcb4efa439084b5e
BlueBunnex <75032462+BlueBunnex@users.noreply.github.com>
2025-07-09 03:47:28 -0400

    Bump detection

--- pickup_object.gd
+++ pickup_object.gd
@@ -2,6 +2,11 @@ extends RigidBody3D
 
 func _ready() -> void:
 	add_to_group("Pickup")
+	
+	# enable collision monitoring
+	contact_monitor = true
+	max_contacts_reported = 1
+	body_entered.connect(on_bump)
 
 func on_pickup():
 	linear_damp = 10
@@ -12,8 +17,8 @@ func while_pickup(force: Vector3):
 func off_pickup():
 	linear_damp = 0
 
-func on_bump(other: RigidBody3D):
-	pass
+func on_bump(bumpee: Node3D):
+	print(bumpee.name)
 
 #mechanic is picking stuff up. when something bumps into something else, an
 #interaction occurs. for example, a bag of 10 carrots seeds being placed onto
