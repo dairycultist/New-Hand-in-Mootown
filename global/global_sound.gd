c72ea2cd549dc9bf958412f0fefc4962b767db6d
BlueBunnex <75032462+BlueBunnex@users.noreply.github.com>
2025-07-14 23:00:10 -0400

    Add seeds sound, improve global sound system

--- global/global_sound.gd
+++ global/global_sound.gd
@@ -1,23 +1,27 @@
 extends Node
 
-var source: AudioStreamPlayer3D
-var listener: Node3D
+var sources: Array
+var available_source_index: int = 0
+
+var listener: Node3D # for position
 
 func _ready() -> void:
 	
-	source = AudioStreamPlayer3D.new()
-	get_node("/root/World").add_child(source)
+	sources = [ AudioStreamPlayer3D.new(), AudioStreamPlayer3D.new(), AudioStreamPlayer3D.new() ]
+	
+	for source in sources:
+		get_node("/root/World").add_child(source)
+	
 	listener = get_node("/root/World/Player")
 
 func play(sound: AudioStreamWAV):
 	
-	source.stream = sound
-	source.volume_linear = 1.0
-	source.pitch_scale = 1.0
-	source.global_position = listener.global_position
-	source.play()
+	play_param(sound, 1.0, 1.0, listener.global_position)
 
 func play_param(sound: AudioStreamWAV, volume: float, pitch: float, position: Vector3):
+	
+	var source: AudioStreamPlayer3D = sources[available_source_index]
+	available_source_index = (available_source_index + 1) % sources.size()
 	
 	source.stream = sound
 	source.volume_linear = volume
